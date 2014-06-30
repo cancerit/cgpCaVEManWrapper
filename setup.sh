deeecf63f15c9aea6c4815f3c05fd9138116bf86
Keiran Raine <keiranmraine@gmail.com>
2014-06-30 11:24:46 +0100

    change mode

--- setup.sh
+++ setup.sh
@@ -2,24 +2,27 @@
 
 ##########LICENCE##########
 #  Copyright (c) 2014 Genome Research Ltd.
-#  
+#
 #  Author: David Jones <cgpit@sanger.ac.uk>
-# 
+#
 #  This file is part of cavemanWrapper.
-# 
+#
 #  cavemanWrapper is free software: you can redistribute it and/or modify it under
 #  the terms of the GNU Affero General Public License as published by the Free
 #  Software Foundation; either version 3 of the License, or (at your option) any
 #  later version.
-# 
+#
 #  This program is distributed in the hope that it will be useful, but WITHOUT
 #  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 #  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
 #  details.
-# 
+#
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##########LICENCE##########
+
+CAVEMAN_CORE="https://github.com/cancerit/CaVEMan/archive/1.2.5.tar.gz"
+SOURCE_SAMTOOLS="https://github.com/samtools/samtools/archive/0.1.19.tar.gz"
 
 
 done_message () {
@@ -49,6 +52,9 @@ if [ "$#" -ne "1" ] ; then
   echo "Please provide an installation path  such as /opt/ICGC"
   exit 0
 fi
+
+CPU=`cat /proc/cpuinfo | egrep "^processor" | wc -l`
+echo "Max compilation CPUs set to $CPU"
 
 INST_PATH=$1
 
@@ -97,3 +103,41 @@ fi
 
 
 #Need to add CaVEMan stuff here... will depend on samtools too (for now).
+
+echo -n "Building samtools ..."
+if [ -e $SETUP_DIR/samtools.success ]; then
+  echo -n " previously installed ...";
+else
+  cd $SETUP_DIR
+  (
+  set -x
+  if [ ! -e samtools ]; then
+    get_distro "samtools" $SOURCE_SAMTOOLS
+  fi
+  make -C samtools -j$CPU
+  touch $SETUP_DIR/samtools.success
+  )>>$INIT_DIR/setup.log 2>&1
+fi
+done_message "" "Failed to build samtools."
+
+SAMTOOLS=$SETUP_DIR/samtools
+
+echo -n "Building CaVEMan ..."
+if [ -e $SETUP_DIR/caveman.success ]; then
+  echo -n " previously installed ...";
+else
+  cd $SETUP_DIR
+  (
+  set -x
+  if [ ! -e caveman ]; then
+    if [ ! -e $INIT_DIR/caveman.tar.gz ]; then
+      get_distro "caveman" $CAVEMAN_CORE
+    else
+      tar --strip-components 1 -C caveman -zxf caveman.tar.gz
+    fi
+  fi
+  make -C caveman -j$CPU
+  touch $SETUP_DIR/caveman.success
+  )>>$INIT_DIR/setup.log 2>&1
+fi
+done_message "" "Failed to build CaVEMan."
